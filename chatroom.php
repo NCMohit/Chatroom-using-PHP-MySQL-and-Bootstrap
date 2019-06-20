d3a9e811f410664bd07ee2901db86475f701d786
N C Mohit <44508697+NCMohit@users.noreply.github.com>
2019-06-20 20:40:25 +0530

    Update chatroom.php

--- chatroom.php
+++ chatroom.php
@@ -14,14 +14,6 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.0/jquery.min.js"></script>
-	<script type="text/javascript">
-	var auto_refresh = setInterval(
-	function ()
-	{
-	$('#load_tweets').load('record_count.php').fadeIn("slow");
-	}, 1000); // refresh every 1000 milliseconds
-	</script>
 </head>
 <body>
 	<div class="container">
